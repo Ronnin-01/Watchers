93dab1d253135540714573799b2b6b10c56d9262
Ronnin-01 <omarmd425@gmail.com>
2024-06-23 23:32:06 +0530

    gradle set up

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,33 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri( "https://jitpack.io") }
     }
 }
+
+//pluginManagement {
+//    repositories {
+//        google()
+//        mavenCentral()
+//        gradlePluginPortal()
+//        maven { url = uri("https://jitpack.io") }
+//    }
+//}
+//
+//dependencyResolutionManagement {
+//    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+//    repositories {
+//        google()
+//        mavenCentral()
+//        maven { url = uri("https://jitpack.io") }
+//    }
+//}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 
